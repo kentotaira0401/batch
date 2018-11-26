9e2d8e42c7b77d392a200416ffb2761cd5cc2a46
saitowataru <wataru.saito@rakus.co.jp>
2018-11-26 17:44:55 +0900

    PutIntoCartServiceの修正

--- src/main/java/jp/co/rakus/ecommerce_b/service/PutItemIntoCartService.java
+++ src/main/java/jp/co/rakus/ecommerce_b/service/PutItemIntoCartService.java
@@ -1,6 +1,5 @@
 package jp.co.rakus.ecommerce_b.service;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,31 +22,36 @@ public class PutItemIntoCartService {
 	private OrderItemRepository orderItemRepository;
 	@Autowired
 	private OrderToppingRepository orderToppingRepository;
-	
-	public void putItemIntoCart(PutItemIntoCartForm form) {
-		
-		
-		Order order = new Order();
-		//セッションから取り出したユーザーIDをorderオブジェクトにセットする
-		order.setUserId(1);
-		order.setStatus(0);
-		order.setTotalPrice(0);
-		order = orderRepository.save(order);
-		OrderItem orderItem = new OrderItem();
-		orderItem.setItemId(form.getIntValueOfItemId());
-		orderItem.setQuantity(form.getIntValueOfQuantity());
-		orderItem.setOrderId(order.getId());
-		orderItem.setSize(form.getSize());
-		orderItem = orderItemRepository.insert(orderItem);
-		
 
-		List<String> toppings = form.getToppingList();
-		for (String topping : toppings) {
-			OrderTopping ordertopping  = new OrderTopping();
-			int toppingId = Integer.parseInt(topping);
-			ordertopping.setToppingId(toppingId);
-			ordertopping.setOrderItemId(orderItem.getId());
-			ordertopping = orderToppingRepository.insert(ordertopping);
+	public void putItemIntoCart(PutItemIntoCartForm form) {
+
+		int userId = 1;
+		int status = 0;
+		List<Order> orderList = orderRepository.findByUserIdAndStatus(userId, status);
+		if (orderList.get(0) == null) {
+			Order order = new Order();
+			// セッションから取り出したユーザーIDをorderオブジェクトにセットする
+			order.setUserId(1);
+			order.setStatus(0);
+			order.setTotalPrice(0);
+			order = orderRepository.save(order);
+		} else {
+			Order order = orderList.get(0);
+			OrderItem orderItem = new OrderItem();
+			orderItem.setItemId(form.getIntValueOfItemId());
+			orderItem.setQuantity(form.getIntValueOfQuantity());
+			orderItem.setOrderId(order.getId());
+			orderItem.setSize(form.getSize());
+			orderItem = orderItemRepository.insert(orderItem);
+			List<String> toppings = form.getToppingList();
+			for (String topping : toppings) {
+				OrderTopping ordertopping = new OrderTopping();
+				int toppingId = Integer.parseInt(topping);
+				ordertopping.setToppingId(toppingId);
+				ordertopping.setOrderItemId(orderItem.getId());
+				ordertopping = orderToppingRepository.insert(ordertopping);
+			}
 		}
+
 	}
 }
