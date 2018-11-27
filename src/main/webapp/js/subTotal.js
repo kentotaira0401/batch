7249b97ddbcbc0c55b92b235e175505d948a5d27
kitano <maiko.kitano@rakus.co.jp>
2018-11-27 14:10:52 +0900

    料金表示のJS

--- src/main/webapp/js/subTotal.js
+++ src/main/webapp/js/subTotal.js
@@ -1,35 +1,36 @@
-
-$(function(){
-	$('.size').change(function(){
-//		var str = $('[name="size"]:checked').val();
-		var str = $('.size:checked').val();
-		console.log(str);
-	});
-});
-
-
-
-//$(function(){
-//	
-//	$('input[name=size]').on("click",function(){
-//			
-//			var sizePrice;
-//			
-//			
-//			if($('size').val() == "M"){
-//				sizePrice=$('item.priceM').val();
-//				console.log(sizePrice);
-//			}else if($('size').val() == "L"){
-//				sizePrice=$('item.priceL').val();
-//				console.log(sizePrice);
-//			}
-//});
-//
-//	$("#get_button").click( function() {
-//	    var radioVal = $("input[name='size']:checked").val();
-//	    console.log(radioVal);
-//	});
-//	
-//});
-
+1	$(function(){
+2	    
+2		    $('.size').change(function(){
+3		//        var str = $('[name="size"]:checked').val();
+4		        var str = $('.size:checked').val();
+5		        console.log(str);
+4	        calc();
+5	    //    var str = $('.size:checked').val();
+6	    //    console.log(str);
+6		    });
+8	    
+9	    $('.toppingList').change(function() {
+10	        calc();
+11+	    //        var cnt = $('.toppingList:checkbox:checked').length;
+12	     //       console.log(cnt);
+13	    });
+14	    
+15	    $('.form-control').change(function() {
+16	        calc();
+17	  //      var quantity = $(this).val();
+18	  //     console.log(quantity);
+19	    });
+20	
+21	    function calc() {
+22	        var str = $('.size:checked').val();
+23	        var cnt = $('.toppingList:checkbox:checked').length;
+24	        var quantity = $('.form-control option:selected').val();
+25	        console.log("サイズ："+ str);
+26	        console.log("トッピング："+ cnt);
+27	        console.log("枚数："+ quantity);
+28	          return "done";
+29	        };
+7	30	});
+8	31	
+9	32	
 
