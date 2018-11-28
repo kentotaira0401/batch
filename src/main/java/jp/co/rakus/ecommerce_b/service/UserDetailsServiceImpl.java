e274b9d24027e5d8d1b08957d87c76815843ff61
nishikiori <yuya.nishikiori@rakus.co.jp>
2018-11-28 15:32:11 +0900

    注文画面

--- src/main/java/jp/co/rakus/ecommerce_b/service/UserDetailsServiceImpl.java
+++ src/main/java/jp/co/rakus/ecommerce_b/service/UserDetailsServiceImpl.java
@@ -42,6 +42,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		if (user == null) {
 			throw new UsernameNotFoundException("そのEmailは登録されていません。");
 		}
+		
+		
+		
+		
 		// 権限付与の例
 		Collection<GrantedAuthority> authorityList = new ArrayList<>();
 		authorityList.add(new SimpleGrantedAuthority("ROLE_USER")); // ユーザ権限付与
