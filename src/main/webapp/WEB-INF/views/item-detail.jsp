1c5a8f0e79b9d017c2adf7847e2b99836b62d390
kitano <maiko.kitano@rakus.co.jp>
2018-11-29 16:26:40 +0900

    カウントアップのテスト

--- src/main/webapp/WEB-INF/views/item-detail.jsp
+++ src/main/webapp/WEB-INF/views/item-detail.jsp
@@ -21,6 +21,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
 </head>
 <body>
 	<div class="container">
@@ -158,6 +159,7 @@
 												<div class="form-group">
 												
 
+				
 							
 													<h2><span id="totalPrice"></span>円(税抜き)</h2>
 												</div>
--- src/main/webapp/js/subTotal.js
+++ src/main/webapp/js/subTotal.js
@@ -16,47 +16,36 @@ $(function(){
 
     function calc() {
     	
-    	var priceM = $('#mPrice').val();
+    	var priceM = $('#mPrice').val(); // Mサイズの価格
     	console.log("Mサイズ価格" + priceM);
     	
-    	var priceL = $('#lPrice').val();
+    	var priceL = $('#lPrice').val(); // Lサイズの価格
     	console.log("Lサイズ価格" + priceL);
     	
-    	var str = $('.size:checked').val();
-    	var cnt = $('.toppingList:checkbox:checked').length;
-    	var quantity = $('.form-control option:selected').val();
+    	var str = $('.size:checked').val(); // サイズ
+    	var cnt = $('.toppingList:checkbox:checked').length;　// トッピングの個数
+    	var quantity = $('.form-control option:selected').val(); // 枚数
     	console.log("サイズ" + str + "トッピング" + cnt + "枚数" + quantity);
-    	
     	
     	
     	var totalPrice = 0;
     	var toppingPrice = 0;
     	
-    	if(str == "M"){
-    		toppingPriceM = (200 * cnt);
-    		totalPrice	= (Number(priceM) + Number(toppingPriceM)) * quantity;
+    	if(str == "M"){ 　　　// Mサイズを選択した際の価格
+    		var toppingPriceM = (200 * cnt);
+    		 totalPrice	= (Number(priceM) + Number(toppingPriceM)) * quantity;
         	console.log("合計：" + totalPrice);
         	
-    	}else if(str == "L"){
-    		toppingPriceL = (300 * cnt);
+    	}else if(str == "L"){　　// Lサイズを選択した際の価格
+    		var toppingPriceL = (300 * cnt);
     		totalPrice = (Number(priceL) + Number(toppingPriceL)) * quantity;
         	console.log("合計：" + totalPrice);
     	}
-    	$("#totalPrice").text(totalPrice.toLocaleString()); 
-    };
     	
+    	$("#totalPrice").text(totalPrice.toLocaleString()); 
+    };   
+   
+
 });
-
-
-
-	
-	
-	
-	
-	
-
-
-
-
 
 
